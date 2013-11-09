57d8e2c5eb1a7650f506a8465a0259f25baffcaf
Kate Ray <email@kateray.net>
2013-11-09 16:28:49 -0500

    check to make sure raml href not already stored

--- chrome/content/essence.js
+++ chrome/content/essence.js
@@ -38,15 +38,18 @@ var myExtension = {
 
   onPageLoad: function(aEvent) {
     var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
-    // do something with the loaded page.
-    // doc.location is a Location object (see below for a link).
-    // You can use it to make your code executed on certain pages only.
-    var raml = [];
-    raml = myExtension.detectRaml(doc);
-    for (i = 0; i < raml.length; i++) {
-        window.content.localStorage['raml-file-' + i.toString()] = raml[i];
+    var ls = window.content.localStorage;
+    var ramlArray = JSON.parse(ls.getItem('essence-raml') || JSON.stringify([]));
+    var newRaml = myExtension.detectRaml(doc);
+
+    for (i = 0; i < newRaml.length; i++) {
+      //check if localstorage already contains href
+      if (ramlArray.indexOf(newRaml[i]) == -1 ){
+        ramlArray.push( newRaml[i] );
+      }
     }
-    console.log(window.content.localStorage);
+
+    ls.setItem('essence-raml', JSON.stringify(ramlArray));
     // add event lfistener for page unload
     aEvent.originalTarget.defaultView.addEventListener("unload", function(event){ myExtension.onPageUnload(event); }, true);
   },
